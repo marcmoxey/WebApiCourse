cad87ee45a0340b2cc4a534cf04d959ab3f41f7a
Marc <moxiem@nychhc.org>
2025-06-27 15:26:56 -0400

    Add version to an api

---
+++ VersionedApiApp/VersionedApi/Controllers/v1/UserController.cs
@@ -0,0 +1,46 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace VersionedApi.Controllers.v1;
+
+// api/v2/Users
+[Route("api/v1/[controller]")]
+[ApiController]
+[ApiVersion("1.0")]
+
+public class UserController : ControllerBase
+{
+    // GET: api/<UserController>
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "Version 2 value1", "Version 2 value2" };
+    }
+
+    // GET api/<UserController>/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return "value";
+    }
+
+    // POST api/<UserController>
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT api/<UserController>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/<UserController>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+}
