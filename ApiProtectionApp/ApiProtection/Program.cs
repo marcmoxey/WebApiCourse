939c84ce0f3ca1cc0b96ab1888acc14b113cf949
Marc <moxiem@nychhc.org>
2025-07-10 13:43:51 -0400

    Add Rate-Limiting

--- ApiProtectionApp/ApiProtection/Program.cs
+++ ApiProtectionApp/ApiProtection/Program.cs
@@ -1,12 +1,18 @@
+using AspNetCoreRateLimit;
+using ApiProtection.StartupConfig;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddResponseCaching();
+
+builder.Services.AddMemoryCache(); // cache call information - per server
+builder.AddRateLimitServices();
 
 var app = builder.Build();
 
@@ -18,11 +24,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseResponseCaching();
-
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseIpRateLimiting();
 
 app.Run();
