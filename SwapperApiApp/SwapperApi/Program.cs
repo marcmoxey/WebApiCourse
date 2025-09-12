d0101129cde0bfe78b351dc5a731559c6052b06f
Marc <moxiem@nychhc.org>
2025-09-12 15:34:35 -0400

    Add CSS

--- SwapperApiApp/SwapperApi/Program.cs
+++ SwapperApiApp/SwapperApi/Program.cs
@@ -44,10 +44,12 @@ if (app.Environment.IsDevelopment())
     {
         opts.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
         opts.RoutePrefix = String.Empty;
+        opts.InjectStylesheet("/css/theme-monokai.css");
     });
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 
 app.UseAuthorization();
 
