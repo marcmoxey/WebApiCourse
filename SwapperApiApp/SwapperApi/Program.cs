9705ec022f775c03100f4a46ae063e2a7e312f51
Marc <moxiem@nychhc.org>
2025-09-12 14:40:45 -0400

    Customize Swagger

--- SwapperApiApp/SwapperApi/Program.cs
+++ SwapperApiApp/SwapperApi/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,14 +7,36 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opts =>
+{
+    opts.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Our User API (this is or title)",
+        Description = "This is the description about our API",
+        TermsOfService = new Uri("https://www.iamtimcorey.com"),
+        Contact = new OpenApiContact
+        {
+            Name = "Tim Corey (Contact Info)",
+            Url = new Uri("https://www.iamtimcorey.com")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Cool License (License Name)",
+            Url = new Uri("https://www.iamtimcorey.com")
+        }
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
+    app.UseSwagger( opts =>
+    {
+        //opts.SerializeAsV2 = true; // V2 OpenApi
+    });
     app.UseSwaggerUI(opts =>
     {
         opts.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
